c9bdec406a9b384f8ed169a765dc3efa9bcf33d9
musab03 <musab.khurram@gmail.com>
2024-11-28 20:57:55 +0500

    tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,8 @@ export default {
     extend: {
       backgroundImage: {
         'custom-gradient': 'linear-gradient(267.36deg, #C2A171 0.09%, #D4B37E 15.51%, #FCDB9B 49.49%, #C2A171 95.32%)',
+        'custom-gradient-hover': 'linear-gradient(267.36deg, #A6855B 0.09%, #B89A6A 15.51%, #E0B681 49.49%, #A6855B 95.32%)',
+
       },
 
       fontFamily:{
