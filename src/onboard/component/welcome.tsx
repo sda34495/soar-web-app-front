defcea64cf7eb3921eb65cafd0052379a25f46bd
Syed Rizwan Hussain <106629183+Newcoderdas@users.noreply.github.com>
2024-11-28 16:07:34 +0500

    n

---
+++ src/onboard/component/welcome.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const welcome = () => {
+  return (
+      <>
+          <div className=''>
+              
+          <h1 className="">
+          SOAR
+              </h1>       
+          </div>
+        
+      </>
+  )
+}
+
+export default welcome
---
+++ src/onboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Welcome from './component/welcome'
+
+const page = () => {
+    return (
+      
+          <Welcome/>
+    
+  )
+}
+
+export default page
