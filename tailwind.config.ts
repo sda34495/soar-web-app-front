e01ae7d465b633e0e8dcd403fa527d0cb01f1a64
musab03 <musab.khurram@gmail.com>
2024-11-28 15:56:06 +0500

    add: splash screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(267.36deg, #C2A171 0.09%, #D4B37E 15.51%, #FCDB9B 49.49%, #C2A171 95.32%)',
+      },
+
+      fontFamily:{
+        'Bricolage-Grotesque': ["Bricolage Grotesque ", 'sans-serif'],
+       
+      
+                                                
+                                                
+                                                
+      },
+
+
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
