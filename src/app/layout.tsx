7e24d3e67af1d5eb7a0791423e842d95f21d2f7a
musab03 <musab.khurram@gmail.com>
2024-12-01 22:53:45 +0500

    remove bg image from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         />
       </Head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-image bg-cover bg-center `}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased bg-image bg-cover bg-center `}
       >
         
         {children}
