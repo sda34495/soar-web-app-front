b504dc9782af4dcbb8ebd12f707c9c2fcac8bb49
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2024-12-13 00:57:33 +0500

    update endpoints

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,6 +1,6 @@
-const LOGIN :string = 'users/login';
+const LOGIN: string = "users/login";
 
-const REGISTER :string = 'users/signup';
+const REGISTER: string = "users/signup";
 
 const GET_CHECK_IN_DATA = "checkin/details";
 
@@ -8,17 +8,47 @@ const GET_DASHBOARD_DATA = "dashboard/details";
 
 const FORGET_PASSWORD_REQUEST_OTP = "users/request-otp";
 
-const  RESET_NEW_PASSWORD = "users/set-new-password";
+const RESET_NEW_PASSWORD = "users/set-new-password";
 
 const OTP_VERIFY = "users/verify-otp";
 
-const Post_Check_IN_DATA = "checkin/add-remove-checkin"
+const Post_Check_IN_DATA = "checkin/add-remove-checkin";
 
-const Book_Coaching_Session  = "sessions/book-coaching-session"
+const Book_Coaching_Session = "sessions/book-coaching-session";
 
-const Post_Referal_Data = "referals/create"
+const Post_Referal_Data = "referals/create";
 
-const Post_Support_Data = 'support/create'
+const Post_Support_Data = "support/create";
 
-export  {GET_DASHBOARD_DATA,FORGET_PASSWORD_REQUEST_OTP, RESET_NEW_PASSWORD ,OTP_VERIFY}
-export  {LOGIN, REGISTER , GET_CHECK_IN_DATA, Post_Check_IN_DATA, Book_Coaching_Session, Post_Referal_Data, Post_Support_Data }
+
+export {
+  LOGIN,
+  REGISTER,
+  FORGET_PASSWORD_REQUEST_OTP,
+  RESET_NEW_PASSWORD,
+  OTP_VERIFY,
+  GET_DASHBOARD_DATA,
+  GET_CHECK_IN_DATA,
+  Post_Check_IN_DATA,
+  Book_Coaching_Session,
+  Post_Referal_Data,
+  Post_Support_Data,
+};
+
+const endpoints = {
+  LOGIN: "users/login",
+  REGISTER: "users/signup",
+  FORGET_PASSWORD_REQUEST_OTP: "users/request-otp",
+  RESET_NEW_PASSWORD: "users/set-new-password",
+  GET_DASHBOARD_DATA: "dashboard/details",
+  GET_CHECK_IN_DATA: "checkin/details",
+  OTP_VERIFY: "users/verify-otp",
+  POST_CHECK_IN_DATA: "checkin/add-remove-checkin",
+  BOOK_COACHING_SESSION: "sessions/book-coaching-session",
+  POST_REFERAL_DATA: "referals/create",
+  POST_SUPPORT_DATA: "support/create",
+};
+
+export default endpoints;
+
+
