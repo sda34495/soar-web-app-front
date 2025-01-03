38cdaf12464731a5469439f315852459caefae6b
musab03 <musab.khurram@gmail.com>
2025-01-03 18:32:56 +0500

    fix: phone number

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -21,7 +21,11 @@ interface FormErrors {
   consultationReason?: string;
 }
 
-const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) => {
+const BookingModal: React.FC<BookingModalProps> = ({
+  isOpen,
+  onClose,
+  price,
+}) => {
   const [showSecondModal, setShowSecondModal] = useState(false); // For the second modal
   const [showPaymentModal, setShowPaymentModal] = useState(false); // For Stripe Payment Modal
   const [formData, setFormData] = useState({
@@ -37,7 +41,9 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
   const [error, setError] = useState<string | null>(null); // For error state
 
   // Handle input changes
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -50,15 +56,18 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
   const validate = (): boolean => {
     const newErrors: FormErrors = {};
 
-    if (!formData.firstName.trim()) newErrors.firstName = "First name is required.";
-    if (!formData.lastName.trim()) newErrors.lastName = "Last name is required.";
+    if (!formData.firstName.trim())
+      newErrors.firstName = "First name is required.";
+    if (!formData.lastName.trim())
+      newErrors.lastName = "Last name is required.";
     if (!formData.email.trim()) {
       newErrors.email = "Email is required.";
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
       newErrors.email = "Invalid email address.";
     }
     if (!formData.phone.trim()) newErrors.phone = "Phone number is required.";
-    if (!formData.consultationReason.trim()) newErrors.consultationReason = "Reason for consultation is required.";
+    if (!formData.consultationReason.trim())
+      newErrors.consultationReason = "Reason for consultation is required.";
 
     setLoginErrors(newErrors);
     return Object.keys(newErrors).length === 0;
@@ -121,13 +130,19 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
               <img src="/laptop.png" alt="Laptop" className="h-16" />
               <span className="border-2 border-golden bg-golden/10 rounded-xl py-2 px-3 text-xl font-semibold">
                 Premium{" "}
-                <span className="text-2xl ml-1 font-Bricolage-Grotesque">${price}</span>
+                <span className="text-2xl ml-1 font-Bricolage-Grotesque">
+                  ${price}
+                </span>
               </span>
             </div>
 
             {/* Title */}
-            <h2 className="text-2xl font-semibold mb-2 font-Bricolage-Grotesque">Booking your session</h2>
-            <p className="text-sm text-gray-400 mb-4">Enter your details for setting up your session</p>
+            <h2 className="text-2xl font-semibold mb-2 font-Bricolage-Grotesque">
+              Booking your session
+            </h2>
+            <p className="text-sm text-gray-400 mb-4">
+              Enter your details for setting up your session
+            </p>
 
             {/* Form */}
             <form className="space-y-4">
@@ -141,7 +156,11 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
                     onChange={handleInputChange}
                     className="w-full px-4 py-2 bg-zinc-600/30 opacity-90 border-[#7c7c7c] rounded-lg placeholder-[#7c7c7c] font-semibold border"
                   />
-                  {loginErrors.firstName && <p className="text-red-500 text-xs mt-1">{loginErrors.firstName}</p>}
+                  {loginErrors.firstName && (
+                    <p className="text-red-500 text-xs mt-1">
+                      {loginErrors.firstName}
+                    </p>
+                  )}
                 </div>
                 <div>
                   <input
@@ -152,7 +171,11 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
                     onChange={handleInputChange}
                     className="w-full px-4 py-2 bg-zinc-600/30 opacity-90 border-[#7c7c7c] rounded-lg placeholder-[#7c7c7c] font-semibold border"
                   />
-                  {loginErrors.lastName && <p className="text-red-500 text-xs mt-1">{loginErrors.lastName}</p>}
+                  {loginErrors.lastName && (
+                    <p className="text-red-500 text-xs mt-1">
+                      {loginErrors.lastName}
+                    </p>
+                  )}
                 </div>
               </div>
               <div className="grid grid-cols-2 gap-4">
@@ -165,7 +188,11 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
                     onChange={handleInputChange}
                     className="w-full px-4 py-2 bg-zinc-600/30 opacity-90 border-[#7c7c7c] rounded-lg placeholder-[#7c7c7c] font-semibold border"
                   />
-                  {loginErrors.email && <p className="text-red-500 text-xs mt-1">{loginErrors.email}</p>}
+                  {loginErrors.email && (
+                    <p className="text-red-500 text-xs mt-1">
+                      {loginErrors.email}
+                    </p>
+                  )}
                 </div>
                 <div>
                   <input
@@ -173,10 +200,21 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
                     placeholder="Phone number"
                     name="phone"
                     value={formData.phone}
-                    onChange={handleInputChange}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (/^\d*$/.test(value) && value.length <= 11) {
+                        // Allow only digits and max length of 15
+                        handleInputChange(e);
+                      }
+                    }}
+                    maxLength={15} // Set max digits allowed
                     className="w-full px-4 py-2 bg-zinc-600/30 opacity-90 border-[#7c7c7c] rounded-lg placeholder-[#7c7c7c] font-semibold border"
                   />
-                  {loginErrors.phone && <p className="text-red-500 text-xs mt-1">{loginErrors.phone}</p>}
+                  {loginErrors.phone && (
+                    <p className="text-red-500 text-xs mt-1">
+                      {loginErrors.phone}
+                    </p>
+                  )}
                 </div>
               </div>
               <div>
@@ -188,7 +226,11 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
                   rows={3}
                   className="w-full px-4 py-2 bg-zinc-600/30 opacity-90 border-[#7c7c7c] rounded-lg placeholder-[#7c7c7c] font-semibold border"
                 ></textarea>
-                {loginErrors.consultationReason && <p className="text-red-500 text-xs mt-1">{loginErrors.consultationReason}</p>}
+                {loginErrors.consultationReason && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {loginErrors.consultationReason}
+                  </p>
+                )}
               </div>
 
               {/* Show general error message if there's any */}
@@ -214,7 +256,6 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose, price }) =
           </div>
         </div>
       )}
-
 
       {/* Payment Modal */}
       {showPaymentModal && (
