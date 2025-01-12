50a7c345aff0987f3f3f73efdfd8da92966d2636
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2025-01-13 02:24:42 +0500

    update post

--- src/app/(dashboard)/community/component/PostCard.tsx
+++ src/app/(dashboard)/community/component/PostCard.tsx
@@ -19,10 +19,9 @@ const PostCard = () => {
   );
   const [activePostId, setActivePostId] = useState(null);
   const [activeComments, setActiveComments] = useState([]);
-  
-  const baseURL = "http://localhost:3000/community/post/";
-  
-  
+
+  const baseURL = `${window.location.origin}/community/post/`;
+
   const [activeSharePostId, setActiveSharePostId] = useState(null);
 
   const [activeReply, setActiveReply] = useState("");
@@ -43,26 +42,24 @@ const PostCard = () => {
 
         // Dispatch posts to Redux
 
-      // Initialize likedPosts based on self_like
-      const initialLikedPosts: { [key: string]: boolean } = {};
-      postsData.forEach((post: any) => {
-        initialLikedPosts[post._id] = post.self_liked; // Use self_like from the server
-      });
-      setLikedPosts(initialLikedPosts);
+        // Initialize likedPosts based on self_like
+        const initialLikedPosts: { [key: string]: boolean } = {};
+        postsData.forEach((post: any) => {
+          initialLikedPosts[post._id] = post.self_liked; // Use self_like from the server
+        });
+        setLikedPosts(initialLikedPosts);
+      }
+    } catch (error) {
+      toast.error(error.message || "Error fetching posts");
     }
-  } catch (error) {
-    toast.error(error.message || "Error fetching posts");
-  }
-}, []);
+  }, []);
 
-const handleCopyLink = (postId: any) => {
-  const shareLink = `${baseURL}${postId}`;
-  navigator.clipboard.writeText(shareLink);
-  toast.success("Link copied to clipboard!");
-  setActiveSharePostId(null);
-  
-
-};
+  const handleCopyLink = (postId: any) => {
+    const shareLink = `${baseURL}${postId}`;
+    navigator.clipboard.writeText(shareLink);
+    toast.success("Link copied to clipboard!");
+    setActiveSharePostId(null);
+  };
   useEffect(() => {
     // fetchPosts();
 
@@ -70,7 +67,11 @@ const handleCopyLink = (postId: any) => {
   }, []);
 
   // Handle like button click
-  const handleLikeClick = async (postId: string,currentLikes: number, selfLiked: boolean) => {
+  const handleLikeClick = async (
+    postId: string,
+    currentLikes: number,
+    selfLiked: boolean
+  ) => {
     if (!postId) {
       console.error("Invalid postId", postId);
       return;
@@ -204,7 +205,9 @@ const handleCopyLink = (postId: any) => {
             {/* Like Button */}
             <div className="flex items-center justify-between mt-4 mr-2">
               <button
-                onClick={() => handleLikeClick(post._id, post.likes, post.self_liked)}
+                onClick={() =>
+                  handleLikeClick(post._id, post.likes, post.self_liked)
+                }
                 className="text-gray-400 text-sm flex items-center cursor-pointer"
               >
                 {likedPosts[post._id] ? (
@@ -229,34 +232,36 @@ const handleCopyLink = (postId: any) => {
                 {post.total_comments >= 2 ? "Comments" : "Comment"}
               </button>
               <div>
-
-              <button className="text-gray-400 hover:text-white flex items-center"
-              onClick={() =>
-                setActiveSharePostId(activeSharePostId === post._id ? null : post._id)
-              }
-              >
-                <RiShareLine className="mr-2" />
-                Share
-              </button>
-              {activeSharePostId === post._id && (
-        <div className="absolute mt-2 bg-[#121212] border border-gray-700 rounded p-2 shadow-md">
-          <div className="flex items-center space-x-2">
-            <input
-              type="text"
-              value={`${baseURL}${post._id}`}
-              readOnly
-              className="text-sm bg-transparent text-white px-2 py-1  w-full opacity-90 border-[#7c7c7c] rounded-xl placeholder-[#7c7c7c] focus:outline-none border"
-              />
-            <button
-              onClick={()=>handleCopyLink(post._id)}
-              className="text-xs text-black bg-custom-gradient  px-3 py-1 rounded"
-              >
-              Copy Link
-            </button>
-          </div>
-          </div>
-      )}
-      </div>
+                <button
+                  className="text-gray-400 hover:text-white flex items-center"
+                  onClick={() =>
+                    setActiveSharePostId(
+                      activeSharePostId === post._id ? null : post._id
+                    )
+                  }
+                >
+                  <RiShareLine className="mr-2" />
+                  Share
+                </button>
+                {activeSharePostId === post._id && (
+                  <div className="absolute mt-2 bg-[#121212] border border-gray-700 rounded p-2 shadow-md">
+                    <div className="flex items-center space-x-2">
+                      <input
+                        type="text"
+                        value={`${baseURL}${post._id}`}
+                        readOnly
+                        className="text-sm bg-transparent text-white px-2 py-1  w-full opacity-90 border-[#7c7c7c] rounded-xl placeholder-[#7c7c7c] focus:outline-none border"
+                      />
+                      <button
+                        onClick={() => handleCopyLink(post._id)}
+                        className="text-xs text-black bg-custom-gradient  px-3 py-1 rounded"
+                      >
+                        Copy Link
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
 
--- src/app/(dashboard)/community/post/[id]/page.tsx
+++ src/app/(dashboard)/community/post/[id]/page.tsx
@@ -13,16 +13,18 @@ import EditOrDeletePost from "../../component/EditOrDeletePost";
 import Comments from "../../component/Comments";
 import ShareModal from "@/components/ShareModal";
 
-
 const PostDetail = () => {
   const { id } = useParams(); // Get params directly
-  const currentUserId = useSelector((state: any) => state.profileSlice.user._id); // Logged-in user ID
+  const currentUserId = useSelector(
+    (state: any) => state.profileSlice.user._id
+  ); // Logged-in user ID
   const [post, setPost] = useState<any>(null);
   const [liked, setLiked] = useState(false);
   const [activeComments, setActiveComments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [commentsVisible, setCommentsVisible] = useState(false); // State to toggle comments visibility
-  const baseURL = "http://localhost:3000/community/post/";
+  const baseURL = `${window.location.origin}/community/post/`;
+  // const baseURL = `'http://localhost:3000/community/post/`;
   const [shareModal, setShareModal] = useState(false);
 
   // Fetch post data by ID
@@ -89,106 +91,124 @@ const PostDetail = () => {
   };
 
   // Toggle comment visibility
-  const handleCommentToggle = () => {
-    setCommentsVisible((prev) => !prev);
+  const handleCommentToggle = async ({ postId }) => {
+    console.log(postId);
+    setCommentsVisible(true);
+    try {
+      const response = await getData(
+        `${endpoints.GET_POST_COMMENTS}?post_id=${postId}`
+      );
+      if (response.data?.success) {
+        setActiveComments(response.data.data);
+        // setActiveReply(response?.data?.data);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
-  
-
 
   if (loading) return <p className="text-center text-white">Loading post...</p>;
   if (!post) return <p className="text-center text-white">Post not found.</p>;
-
+  console.log("Post data", post);
   return (
-    <>
-    <div className="flex flex-col items-center p-6 text-white space-y-6">
-      <div className="flex flex-col bg-[#121212] p-6 shadow-lg rounded-lg max-w-3xl w-full">
-        <div className="flex items-start justify-between mb-4">
-          <div className="flex items-center space-x-4">
-            <img
-              src={post?.user?.profile_url || "/avatar.jpeg"}
-              alt={post.user?.user_name || "User"}
-              className="rounded-full w-10 h-10 object-cover"
-            />
-            <div>
-              <p className="font-bold">{post.user?.user_name || "User"}</p>
-              <p className="text-sm text-gray-400">{new Date(post.createdAt).toLocaleString()}</p>
+    <div className=" flex p-6 space-y-8 w-auto">
+      <div className="flex  items-center justify-center p-6 text-white space-y-6 ">
+        <div className="flex flex-col bg-[#121212] p-6 shadow-lg rounded-lg max-w-3xl w-full h-full">
+          <div className="flex items-start justify-between mb-4">
+            <div className="flex items-center space-x-4">
+              <img
+                src={post?.user?.profile_url || "/avatar.jpeg"}
+                alt={post.user?.user_name || "User"}
+                className="rounded-full w-10 h-10 object-cover"
+              />
+              <div>
+                <p className="font-bold">{post.user?.user_name || "User"}</p>
+                <p className="text-sm text-gray-400">
+                  {new Date(post.createdAt).toLocaleString()}
+                </p>
+              </div>
+            </div>
+            <div className="relative">
+              {post.user_id === currentUserId && (
+                <EditOrDeletePost post_id={post._id} postData={post} />
+              )}
             </div>
           </div>
-          <div className="relative">
 
-          {post.user_id === currentUserId && (
-            <EditOrDeletePost post_id={post._id} postData={post} />
-            )}
+          <h2 className="text-lg font-semibold">{post.header}</h2>
+          <p className="text-gray-400 mt-2">{post.description}</p>
+
+          {post.media && (
+            <div className="mt-4">
+              <img
+                src={post.media}
+                alt="Post media"
+                className="rounded-lg w-full max-w-[640px] h-full object-cover mx-auto"
+              />
             </div>
-        </div>
+          )}
 
-        <h2 className="text-lg font-semibold">{post.header}</h2>
-        <p className="text-gray-400 mt-2">{post.description}</p>
+          {/* Like, Comment, Share */}
+          <div className="flex items-center justify-between mt-4">
+            <button
+              onClick={handleLikeClick}
+              className="flex items-center text-gray-400 text-sm hover:text-white"
+            >
+              {liked ? (
+                <IoMdHeart className="mr-2 text-white" />
+              ) : (
+                <IoMdHeartEmpty className="mr-2" />
+              )}
+              {post.likes.toLocaleString()} Likes
+            </button>
 
-        {post.media && (
-          <div className="mt-4">
-            <img
-              src={post.media}
-              alt="Post media"
-              className="rounded-lg w-full max-w-[640px] object-cover mx-auto"
-            />
+            <button
+              onClick={() => handleCommentToggle({ postId: post._id })} // Handle comment toggle
+              className={`text-sm flex items-center ${
+                post.allow_comments
+                  ? "text-gray-400 hover:text-white"
+                  : "text-gray-600 cursor-not-allowed"
+              }`}
+              disabled={!post.allow_comments}
+            >
+              <IoChatbubbleEllipsesOutline className="mr-2" />
+              {post.total_comments}{" "}
+              {post.total_comments === 1 ? "Comment" : "Comments"}
+            </button>
+
+            <button
+              onClick={handleCopyLink}
+              className="text-gray-400 hover:text-white flex items-center"
+            >
+              <RiShareLine className="mr-2" />
+              Share
+            </button>
           </div>
-        )}
-
-        {/* Like, Comment, Share */}
-        <div className="flex items-center justify-between mt-4">
-          <button
-            onClick={handleLikeClick}
-            className="flex items-center text-gray-400 text-sm hover:text-white"
-          >
-            {liked ? (
-              <IoMdHeart className="mr-2 text-white" />
-            ) : (
-              <IoMdHeartEmpty className="mr-2" />
-            )}
-            {post.likes.toLocaleString()} Likes
-          </button>
-
-          <button
-            onClick={handleCommentToggle} // Handle comment toggle
-            className={`text-sm flex items-center ${
-              post.allow_comments ? "text-gray-400 hover:text-white" : "text-gray-600 cursor-not-allowed"
-            }`}
-            disabled={!post.allow_comments}
-          >
-            <IoChatbubbleEllipsesOutline className="mr-2" />
-            {post.total_comments} {post.total_comments === 1 ? "Comment" : "Comments"}
-          </button>
-
-          <button onClick={handleCopyLink} className="text-gray-400 hover:text-white flex items-center">
-            <RiShareLine className="mr-2" />
-            Share
-          </button>
-          
         </div>
-        <div>
-
-      {commentsVisible && post.allow_comments && (
-        <Comments
-        postData={post}
-        activePostId={post._id}
-        activeComments={activeComments}
-        setActiveComments={setActiveComments} setActivePostId={undefined} handleCommentToggle={handleCommentToggle}        />
-        )}
-       
+        <div className="">
+          {commentsVisible && post.allow_comments && (
+            <Comments
+              postData={post}
+              activePostId={post._id}
+              activeComments={activeComments}
+              setActiveComments={setActiveComments}
+              setActivePostId={setCommentsVisible}
+              handleCommentToggle={handleCommentToggle}
+            />
+          )}
         </div>
-
-        
-            
-       
       </div>
-
       {/* Comments Section */}
-      
+
+      <ShareModal
+        title={"Share Post"}
+        description={"Copy the link or Share via Social plattform"}
+        isOpen={shareModal}
+        image_url={"/copy.svg"}
+        linkToShare={`${baseURL}/${id}`}
+        onClose={() => setShareModal(false)}
+      />
     </div>
-    <ShareModal title={"Share Post"} description={"Copy the link or Share via Social plattform"} isOpen={shareModal} image_url={"/copy.svg"} linkToShare={`${baseURL}/${id}`} onClose={()=>setShareModal(false)}
-    />
-    </>
   );
 };
 
