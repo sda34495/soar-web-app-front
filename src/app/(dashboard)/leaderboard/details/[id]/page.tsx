69d062b7fc2d0191888d92fcb9732aaa495fc40a
musab03 <musab.khurram@gmail.com>
2025-01-13 21:46:08 +0500

    remove: /2000 from card

--- src/app/(dashboard)/leaderboard/details/[id]/page.tsx
+++ src/app/(dashboard)/leaderboard/details/[id]/page.tsx
@@ -125,12 +125,12 @@ export default function UserDetailPage({ params }: { params: Promise<{ id: strin
               <div className="flex items-center  justify-between">
                 <span className="font-semibold text-gray-300">Daily Check-ins</span>
                 <span className="font-semibold text-xl">
-                  {userData.daily_check_ins.toLocaleString()}/2000
+                  {userData.daily_check_ins.toLocaleString()}
                 </span>
               </div>
               <div className="flex items-center justify-between">
                 <span className="font-semibold text-gray-300">Weekly Check-ins</span>
-                {userData.weekly_check_ins} / 100
+                {userData.weekly_check_ins}
               </div>
             </div>
 
