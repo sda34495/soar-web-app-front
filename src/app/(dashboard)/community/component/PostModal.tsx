feca2bb49d4e377432acd237b72dc1471df76b0b
musab03 <musab.khurram@gmail.com>
2025-02-03 23:05:50 +0500

    fix: modals

--- src/app/(dashboard)/community/component/PostModal.tsx
+++ src/app/(dashboard)/community/component/PostModal.tsx
@@ -5,15 +5,16 @@ const PostModal = ({ isOpen,  children, title, description }: any) => {
   return (
     <div>
       {isOpen && (
-        <div className="overflow-y-auto overflow-x-hidden fixed inset-0 flex items-center justify-center z-50 w-full h-screen bg-gray-700 bg-opacity-50 backdrop-blur-sm">
-          <div className="relative p-4 w-full max-w-lg h-full md:h-auto mx-4 my-auto">
-            <div className="relative p-4 bg-[#202020] rounded-3xl shadow-md md:p-8 border border-[#7c7c7c] ">
-              <h3 className="mb-2 text-2xl font-bold text-white">{title}</h3>
-              <p className="text-xs text-[#BDBDBD]">{description}</p>
-              {children}
-            </div>
-          </div>
-        </div>
+       <div className="overflow-y-auto overflow-x-hidden fixed inset-0 flex items-center justify-center z-50 w-full h-screen bg-gray-700 bg-opacity-50 backdrop-blur-sm">
+       <div className="relative p-4 w-full max-w-lg h-auto md:h-auto mx-4 my-auto flex justify-center">
+         <div className="relative p-4 bg-[#202020] rounded-3xl shadow-md md:p-8 border border-[#7c7c7c] w-full max-w-lg mx-auto">
+           <h3 className="mb-2 text-2xl font-bold text-white">{title}</h3>
+           <p className="text-xs text-[#BDBDBD]">{description}</p>
+           {children}
+         </div>
+       </div>
+     </div>
+     
       )}
     </div>
   );
